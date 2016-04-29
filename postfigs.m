2dbd7a31118cfbc48797e30c33040b2231f2b90d
cedavidyang <ynyang1988@yahoo.com>
2016-04-29 09:42:17 +0800

    change font size

--- postfigs.m
+++ postfigs.m
@@ -4,41 +4,47 @@ function [] = postfigs( hfig, fmt)
 if strcmpi(fmt, 'asce')
     % typical figure size 3.5in (336 pixels)
     set(hfig, 'Position', [100, 100, 355, 266])
-    ax = get(hfig,'CurrentAxes');
+    ax = get(hfig,'CurrentAxes');       
+    % change axis font
+    set(ax,'fontsize',10);
+    set(ax,'fontname','times new roman');
+    % change label font
     try
-        % change label font
         xlb = get(ax, 'xlabel');
-        set(xlb, 'fontsize', 8);
-        set(xlb, 'Fontname', 'times new roman');
+        set(xlb, 'fontsize', 10, 'Fontname', 'times new roman');
+    catch
+        disp 'no xlabel';
+    end    
+    try
         ylb = get(ax, 'ylabel');
-        set(ylb, 'fontsize', 8);
-        set(ylb, 'Fontname', 'times new roman');   
-        % change axis font
-        set(ax,'fontsize',8);
-        set(ax,'fontname','times new roman');
-        % change legend font
-        lgd = legend(ax);
-        set(lgd, 'fontsize', 8);
-        set(lgd, 'fontname', 'times new roman');
-        % change annotation font
-        % get the handle of the hidden annotation axes
-        hAnnotAxes = findall(hfig,'Tag','scribeOverlay');
-        % get its children handles
-        if ~isempty(hAnnotAxes)
-            hAnnotChildren = get(hAnnotAxes,'Children');
-            for i = 1:length(hAnnotChildren)
+        set(ylb, 'fontsize', 10, 'Fontname', 'times new roman');
+    catch
+        disp 'no ylabel';
+    end     
+    % change legend font
+    lgd = legend(ax);
+    try
+        set(lgd, 'fontsize', 10, 'fontname', 'times new roman');
+    catch
+        disp 'no legend';
+    end
+    % get the handle of the hidden annotation axes
+    hAnnotAxes = findall(hfig,'Tag','scribeOverlay');
+    try
+        hAnnotChildren = get(hAnnotAxes,'Children');
+        for i = 1:length(hAnnotChildren)
                 ha = hAnnotChildren(i);
                 set(ha, 'fontsize', 8)
                 set(ha, 'fontname', 'times new roman')
                 set(ha, 'headlength', 6)
                 set(ha, 'headwidth', 6)
-            end
         end
     catch
-        disp 'no xaxis';
+        disp 'no annotation';
     end
-    % save figures
+
 end
+% save figures
 name = input('figure name:?', 's');
 set(hfig, 'paperpositionmode', 'auto');
 savefig(hfig, strcat('./fig/', name))
