0266f36fc050d234451906491c3709cca4e31436
cedavidyang <ynyang1988@yahoo.com>
2017-02-07 12:03:46 -0500

    only figure width is needed as input

--- postfigs.m
+++ postfigs.m
@@ -1,50 +1,50 @@
-function [] = postfigs( hfig, fmt, issave, ftsize)
+function [] = postfigs( hfig, figsize, issave, ftsize)
 % hfig: handle of fig
 % format: fig format
-if strcmpi(fmt, 'asce')
+if strcmpi(num2str(figsize), 'asce')
     % typical figure size 3.5in (336 pixels)
-%     set(hfig, 'Position', [100, 100, 355, 266])
-    set(hfig, 'Position', [100, 100, 336, 252])
-    get(hfig,'CurrentAxes');   % not sure whether the first "get" give a diffent value
-    ax = get(hfig,'CurrentAxes');
-    % change axis font
-    set(ax,'fontsize',ftsize);
-    set(ax,'fontname','times new roman');
-    % change label font
-    try
-        xlb = get(ax, 'xlabel');
-        set(xlb, 'fontsize', ftsize, 'Fontname', 'times new roman');
-    catch
-        disp 'no xlabel';
-    end    
-    try
-        ylb = get(ax, 'ylabel');
-        set(ylb, 'fontsize', ftsize, 'Fontname', 'times new roman');
-    catch
-        disp 'no ylabel';
-    end     
-    % change legend font
-    lgd = legend(ax);
-    try
-        set(lgd, 'fontsize', ftsize, 'fontname', 'times new roman');
-    catch
-        disp 'no legend';
+    set(hfig, 'Position', [100, 100, 336/0.75, 336])
+else
+    set(hfig, 'Position', [100, 100, figsize/0.75, figsize])
+end
+get(hfig,'CurrentAxes');   % not sure whether the first "get" give a diffent value
+ax = get(hfig,'CurrentAxes');
+% change axis font
+set(ax,'fontsize',ftsize);
+set(ax,'fontname','times new roman');
+% change label font
+try
+    xlb = get(ax, 'xlabel');
+    set(xlb, 'fontsize', ftsize, 'Fontname', 'times new roman');
+catch
+    disp 'no xlabel';
+end
+try
+    ylb = get(ax, 'ylabel');
+    set(ylb, 'fontsize', ftsize, 'Fontname', 'times new roman');
+catch
+    disp 'no ylabel';
+end
+% change legend font
+lgd = legend(ax);
+try
+    set(lgd, 'fontsize', ftsize, 'fontname', 'times new roman');
+catch
+    disp 'no legend';
+end
+% get the handle of the hidden annotation axes
+hAnnotAxes = findall(hfig,'Tag','scribeOverlay');
+try
+    hAnnotChildren = get(hAnnotAxes,'Children');
+    for i = 1:length(hAnnotChildren)
+        ha = hAnnotChildren(i);
+        set(ha, 'fontsize', ftsize)
+        set(ha, 'fontname', 'times new roman')
+        set(ha, 'headlength', 6)
+        set(ha, 'headwidth', 6)
     end
-    % get the handle of the hidden annotation axes
-    hAnnotAxes = findall(hfig,'Tag','scribeOverlay');
-    try
-        hAnnotChildren = get(hAnnotAxes,'Children');
-        for i = 1:length(hAnnotChildren)
-                ha = hAnnotChildren(i);
-                set(ha, 'fontsize', ftsize)
-                set(ha, 'fontname', 'times new roman')
-                set(ha, 'headlength', 6)
-                set(ha, 'headwidth', 6)
-        end
-    catch
-        disp 'no annotation';
-    end
-
+catch
+    disp 'no annotation';
 end
 
 if issave == true
